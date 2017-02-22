90217da37d8d7bbc8790c3cae996b744eee6978b
Gene Chuang <gc.genechuang@gmail.com>
2017-02-22 07:51:21 -0500

    Removed test comment from Customer_View.java. Testing Webhook on GitHub for GitHub to Jenkins push

--- src/com/gene/sample/Customer_View.java
+++ src/com/gene/sample/Customer_View.java
@@ -10,7 +10,6 @@ import items.AbstractItem;
 public class Customer_View {
 
 	public static void main(String[] args) throws IOException {
-		//test
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		Customer_View customer = new Customer_View();
